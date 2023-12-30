0bd101f69768146642e9856b322e83e6cd1fa06d
Shivank Sharma <sharmashivank905@gamil.com>
2023-12-30 10:43:59 +0530

    version 3.0

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,13 +11,10 @@ function Contact() {
   const handleSubmit = (e) => {
     e.preventDefault();
     toast.success("Message send")
-    // Access form values using formRef.current
-    const name = formRef.current.formName.value;
-    const email = formRef.current.formEmail.value;
-    const message = formRef.current.formMessage.value;
-    // You can now handle the form data as needed
+    const name = formRef.current.from_name.value;
+    const email = formRef.current.to_name.value;
+    const message = formRef.current.message.value;
     console.log(name, email, message);
-
 
     e.preventDefault();
 
@@ -30,55 +27,55 @@ function Contact() {
   };
 
   return (
-    <div style={{backgroundImage: `url(${bg})`, backgroundSize: 'cover'}} className='herosection pt-[4.5rem]  h-screen'>
-      <div className="form-page">
+    <div style={{backgroundImage: `url(${bg})`, backgroundSize: 'cover'}} className='unique-herosection pt-[4.5rem] h-screen  unique-body'>
+      <div className="unique-form-page">
         <div className='flex my-[5rem] shadow-2xl bg-slate-50 rounded-3xl'>
           <img className='' src={img} alt="" />
           <form
             ref={formRef}
             onSubmit={handleSubmit}
-            className='shadow-2xl border-2 scale-105 border-black bg-gradient-to-r from-orange-300 to-rose-300 rounded-3xl'
+            className='unique-form shadow-2xl border-2 scale-105 border-black bg-gradient-to-r from-orange-300 to-rose-300 rounded-3xl'
           >
             <h1 className="title text-center mb-4">Contact Us</h1>
 
             {/* Name */}
-            <div className="form-group position-relative">
-              <label htmlFor="formName" className="d-block">
+            <div className="unique-form-group position-relative">
+              <label htmlFor="unique-formName" className="d-block">
                 <i className="icon" data-feather="user"></i>
               </label>
               <input
                 type="text"
-                id="formName"
+                id="unique-formName"
                 name="from_name"
-                className="form-control form-control-lg thick"
+                className="unique-form-control w-[320px] rounded-full py-2 px-4 unique-form-control-lg thick"
                 placeholder="Name"
                 required
               />
             </div>
 
             {/* E-mail */}
-            <div className="form-group position-relative">
-              <label htmlFor="formEmail" className="d-block">
+            <div className="unique-form-group position-relative">
+              <label htmlFor="unique-formEmail" className="d-block">
                 <i className="icon" data-feather="mail"></i>
               </label>
               <input
                 type="email"
-                id="formEmail"
+                id="unique-formEmail"
                 name="to_name"
-                className="form-control form-control-lg thick"
+                className="unique-form-control w-[320px] rounded-full py-2 px-4 unique-form-control-lg thick"
                 placeholder="E-mail"
                 required
               />
             </div>
 
             {/* Message */}
-            <div className="form-group message">
+            <div className="unique-form-group message">
               <textarea
-                id="formMessage"
+                id="unique-formMessage"
                 name="message"
-                className="form-control form-control-lg"
+                className="unique-form-control rounded-3xl py-2 px-4 unique-form-control-lg"
                 rows="7"
-                placeholder="    Message"
+                placeholder="Message"
                 required
               ></textarea>
             </div>
