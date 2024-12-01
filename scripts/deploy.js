1ae32441cc729fae2948ade8ffd73ba3187e8016
Shivank Sharma <shivanks224@gmail.com>
2024-12-01 22:29:53 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,13 +2,13 @@
 
 async function main() {
   const Doc_verification = await ethers.getContractFactory('DocVerification')
-  // const doc_ver = await Doc_verification.deploy(
-  //   '0xae85ac81cAa5b9D745cC690f890F6eA68c15AA10'
-  // )
-
   const doc_ver = await Doc_verification.deploy(
-    '0x6743cC9aFcD7E08417b40484D4D3395fd2102Aae'
+    '0xae85ac81cAa5b9D745cC690f890F6eA68c15AA10'
   )
+
+  // const doc_ver = await Doc_verification.deploy(
+  //   '0x6743cC9aFcD7E08417b40484D4D3395fd2102Aae'
+  // )
   console.log('Goverment address : ', doc_ver.address)
 }
 
