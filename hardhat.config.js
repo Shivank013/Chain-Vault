83c20e041a28fe1cdf502f503656430a5974fb95
Shivank Sharma <shivanks224@gmail.com>
2024-12-01 22:13:50 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,8 @@ require('@nomiclabs/hardhat-waffle')
 require('@nomiclabs/hardhat-ethers')
 
 const MYGANACHE_PRIVATE_KEY ='0x07a7e4b73d0e3c93764db1ad03a4cc1eb3961427290f3fc12e338ed13459f622'
-const PRIVATE_KEY = '15b3c0cd3ac484b776c2b1c628c497c8d1d8080cad4e808de9600a750a57df6a'
+// const PRIVATE_KEY = '15b3c0cd3ac484b776c2b1c628c497c8d1d8080cad4e808de9600a750a57df6a'
+const PRIVATE_KEY = "4c868fb3782097115a64b7042547a20c21cb84409c48ab046f9f19c6612e23ab"
 module.exports = {
   solidity: '0.8.19',
 
